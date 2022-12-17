16e0562a7322feaec63fb3a856b3b14edf4b3ad0
irishabhnegi <91733853+irishabhnegi@users.noreply.github.com>
2022-12-17 16:02:09 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+// CommonJS , every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+require('./7-mind-grenade')
